3f99496cd1a1103c3f501bd5ba8f34a90c96f52c
cmontenegro <cmontenegro>
2024-01-27 16:41:25 -0500

    controller program

--- Flight.AirService.Solution/Flight.AirService.Api/Program.cs
+++ Flight.AirService.Solution/Flight.AirService.Api/Program.cs
@@ -1,4 +1,20 @@
+using Flight.AirService.AccessData.Data;
+using Flight.AirService.AccessData.Init;
+using Flight.AirService.AccessData.ObjectRepository.Implementation;
+using Flight.AirService.AccessData.ObjectRepository.Interface;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 
@@ -6,6 +22,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("AirServiceConectionDB");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IDBInitializer, DBInitializer>();
+builder.Services.AddScoped<ITransportRepository, TransportRepository>();
+builder.Services.AddScoped<IJourneyRepository, JourneyRepository>();
+builder.Services.AddScoped<IFlightDTLRepository, FlightDTLRepository>();
 
 var app = builder.Build();
 
@@ -19,6 +43,25 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowOrigin");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+    try
+    {
+        var initializer = services.GetRequiredService<IDBInitializer>();
+        initializer.Initialize();
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "Error in migration");
+    }
+}
 
 app.MapControllers();
 
